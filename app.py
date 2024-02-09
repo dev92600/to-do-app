769fe8976e4001e75b363c266998769ddadb7449
Divine Samba <Divinesambaa@gmail.com>
2024-02-09 15:32:59 +0000

    first

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# new instance of the Flask class
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(300), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+@app.route("/")
+def home():
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template("index.html", todo_list = todo_list)
+
+@app.route("/add", methods= ["POST"])
+def add():
+    task = request.form.get("task")
+    new_todo = Todo(task=task)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+@app.route("/update/<todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.completed = not todo.completed
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=8000)
